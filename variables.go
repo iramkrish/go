256b26ac3277b1fb38365d20df5894aa609d90d9
ram krishnan <iramkrish95@gmail.com>
2025-06-18 14:13:16 +0530

    lesson1: variables

---
+++ variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	// string
+	var name string = "ram"
+	lastName := "krishnan"
+	fmt.Println(name,lastName)
+
+	// int
+	var a,b int = 10 , 20
+	fmt.Println(a,b)
+
+	// default
+	var stringOne string // ""
+	var numberOne int // 0
+	var boolOne bool // false
+	fmt.Println(stringOne, numberOne, boolOne)
+
+	// const
+	// const needs to be initialized , no default value and canot be reassigned
+	const constValue int = 90
+	fmt.Println(constValue)
+}
